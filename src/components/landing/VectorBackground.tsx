2add90489e231f42edb7a72f4d566fdccd2f3b46
[dyad] <saikat@thynknext.in>
2025-10-02 07:47:50 +0530

    [dyad] Updated background graphics to a professional style - wrote 1 file(s)

--- src/components/landing/VectorBackground.tsx
+++ src/components/landing/VectorBackground.tsx
@@ -2,33 +2,29 @@
 
 import React from 'react';
 import {
-  Briefcase,
-  LineChart,
-  Globe,
-  Users,
-  Target,
-  Mail,
-  Bot,
-  Building,
-  Handshake,
-  Lightbulb,
-  BarChart,
-  PieChart,
-} from 'lucide-react';
+  FaHandshake,
+  FaGlobe,
+  FaMoneyBillWave,
+  FaBuilding,
+  FaChartLine,
+  FaBullseye,
+  FaLightbulb,
+  FaUsers,
+  FaChartPie,
+  FaEnvelope,
+} from 'react-icons/fa6';
 
 const icons = [
-  { Icon: Briefcase, top: '10%', left: '5%', size: 24 },
-  { Icon: LineChart, top: '20%', left: '80%', size: 32 },
-  { Icon: Globe, top: '70%', left: '10%', size: 28 },
-  { Icon: Users, top: '80%', left: '90%', size: 24 },
-  { Icon: Target, top: '5%', left: '50%', size: 20 },
-  { Icon: Mail, top: '90%', left: '40%', size: 20 },
-  { Icon: Bot, top: '50%', left: '50%', size: 40 },
-  { Icon: Building, top: '30%', left: '20%', size: 28 },
-  { Icon: Handshake, top: '60%', left: '70%', size: 32 },
-  { Icon: Lightbulb, top: '40%', left: '95%', size: 24 },
-  { Icon: BarChart, top: '95%', left: '5%', size: 28 },
-  { Icon: PieChart, top: '5%', left: '90%', size: 20 },
+  { Icon: FaHandshake, top: '10%', left: '5%', size: 28 },
+  { Icon: FaGlobe, top: '20%', left: '80%', size: 32 },
+  { Icon: FaMoneyBillWave, top: '70%', left: '10%', size: 32 },
+  { Icon: FaUsers, top: '80%', left: '90%', size: 28 },
+  { Icon: FaBullseye, top: '5%', left: '50%', size: 24 },
+  { Icon: FaEnvelope, top: '90%', left: '40%', size: 24 },
+  { Icon: FaBuilding, top: '30%', left: '20%', size: 28 },
+  { Icon: FaLightbulb, top: '40%', left: '95%', size: 24 },
+  { Icon: FaChartLine, top: '95%', left: '5%', size: 28 },
+  { Icon: FaChartPie, top: '5%', left: '90%', size: 24 },
 ];
 
 const VectorBackground = () => {
